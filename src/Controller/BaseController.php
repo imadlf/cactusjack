decd2c42ed68ae8372542a524ef3028c07db5968
Imad <imad.bennaceur1234@gmail.com>
2024-03-23 00:24:30 +0100

    test bostrap

--- src/Controller/BaseController.php
+++ src/Controller/BaseController.php
@@ -2,7 +2,11 @@
 
 namespace App\Controller;
 
+use App\Entity\Contact;
+use App\Form\ContactType;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -12,6 +16,27 @@ class BaseController extends AbstractController
     public function index(): Response
     {
         return $this->render('base/index.html.twig', [
+        ]);
+    }
+    #[Route('/contact', name: 'app_contact')]
+    public function contact(Request $request, EntityManagerInterface $em): Response
+    {
+        $contact = new Contact();
+        $form = $this->createForm(ContactType::class, $contact);
+
+        if ($request->isMethod('POST')) {
+            $form->handleRequest($request);
+            if ($form->isSubmitted() && $form->isValid()) {
+                $contact->setDateEnvoi(new \Datetime());
+                $em->persist($contact);
+                $em->flush();
+                $this->addFlash('notice', 'Message envoyé');
+                return $this->redirectToRoute('app_contact');
+            }
+        }
+
+        return $this->render('base/contact.html.twig', [
+            'form' => $form->createView(),
         ]);
     }
 }
